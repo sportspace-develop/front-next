b6e8d846f29df813031b7993ec8388fc6bfa04ad
Albina Valiullina <avaliullina@mfms.ru>
2024-10-07 15:34:06 +0300

    добавила настройки, чтобы бек работал

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*', // Прокси всех запросов, которые начинаются с /api/v1
+        destination: 'http://localhost:8080/api/v1/:path*', // Адрес бэкенда
+      },
+    ];
+  },
+};
 
 export default nextConfig;
