cbec3c5abd20419ae6aa5e0505383a014776ec15
Albina Valiullina <albi.valiullina@yandex.ru>
2025-02-22 15:11:11 +0300

    added lib - i18n

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,29 @@
-import type { NextRequest } from 'next/server';
+import createMiddleware from 'next-intl/middleware';
+import { NextRequest, NextResponse } from 'next/server';
 
-import { paths } from '@/paths';
+import { routing } from '@/i18n/routing';
+
+const intlMiddleware = createMiddleware(routing);
 
 export function middleware(request: NextRequest) {
+  const response = intlMiddleware(request);
+
   const currentUser = request.cookies.get('token')?.value;
+  const pathname = request.nextUrl.pathname;
 
-  if (currentUser && !request.nextUrl.pathname.startsWith('/dashboard')) {
-    return Response.redirect(new URL(paths.dashboard.teams.index, request.url));
+  if (currentUser && !pathname.includes('/dashboard')) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 
-  if (!currentUser && !request.nextUrl.pathname.startsWith('/auth')) {
-    return Response.redirect(new URL(paths.auth.signIn, request.url));
+  if (!currentUser && !pathname.includes('/auth')) {
+    return NextResponse.redirect(new URL('/auth/sign-in', request.url));
   }
+
+  return response;
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.ico|.*\\.svg|.*\\.png|.*\\.jpeg|.*\\.jpg).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|.*\\.(?:svg|png|jpg|jpeg)).*)',
+  ],
 };
